b4508402c26b331774d98daa4a9f9b75cfd00c95
Michael Newbold <91293169+MichaelENewboldprivate@users.noreply.github.com>
2024-11-13 23:29:25 -0800

    list working like list test

--- lib/screens/general/screen_list.dart
+++ lib/screens/general/screen_list.dart
@@ -3,9 +3,22 @@ import 'package:go_router/go_router.dart';
 import 'package:csc322_starter_app/widgets/general/widget_food_item.dart';
 import 'package:csc322_starter_app/widgets/general/widget_navigation_bar.dart';
 
-class ScreenList extends StatelessWidget {
+class FoodItem {
+  final String name;
+  final String description;
+  bool isChecked; // New property to track checked state
+
+  FoodItem({required this.name, required this.description, this.isChecked = false});
+}
+
+class ScreenList extends StatefulWidget {
   static const String routeName = '/lists';
 
+  @override
+  _ScreenListState createState() => _ScreenListState();
+}
+
+class _ScreenListState extends State<ScreenList> {
   final List<FoodItem> foodItems = [
     FoodItem(name: 'Pizza', description: 'Cheese and tomato pizza'),
     FoodItem(name: 'Burger', description: 'Beef burger with lettuce and cheese'),
@@ -13,6 +26,18 @@ class ScreenList extends StatelessWidget {
     FoodItem(name: 'Pasta', description: 'Spaghetti with marinara sauce'),
     FoodItem(name: 'Salad', description: 'Mixed greens with vinaigrette dressing'),
   ];
+
+  void _toggleCheck(int index) {
+    setState(() {
+      foodItems[index].isChecked = !foodItems[index].isChecked;
+    });
+  }
+
+  void _deleteItem(int index) {
+    setState(() {
+      foodItems.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,18 +48,21 @@ class ScreenList extends StatelessWidget {
           children: [
             const DrawerHeader(
               decoration: BoxDecoration(
-                color:Colors.tealAccent,
+                color: Colors.tealAccent,
               ),
               child: Text('Lists'),
             ),
-
-            ListTile( 
-              title: const Text('Recipes'), onTap: () {}), 
-            ListTile( 
-              title: const Text('List1'), onTap: () {})
-          ]
-         )
-       ),
+            ListTile(
+              title: const Text('Recipes'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('List1'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -44,9 +72,23 @@ class ScreenList extends StatelessWidget {
                 itemCount: foodItems.length,
                 itemBuilder: (context, index) {
                   return ListTile(
+                    leading: Checkbox(
+                      value: foodItems[index].isChecked,
+                      onChanged: (value) {
+                        _toggleCheck(index);
+                      },
+                    ),
                     title: Text(foodItems[index].name),
                     subtitle: Text(foodItems[index].description),
-                    onTap: () {},
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        _deleteItem(index);
+                      },
+                    ),
+                    onTap: () {
+                      _toggleCheck(index);
+                    },
                   );
                 },
               ),
