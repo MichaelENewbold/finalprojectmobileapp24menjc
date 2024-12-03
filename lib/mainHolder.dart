77af682d1cf53abbcfc10eb8d4977e2fe9815836
Michael Newbold <91293169+MichaelENewboldprivate@users.noreply.github.com>
2024-12-02 21:49:46 -0800

    updated Gradle restarting work on Firebase

---
+++ lib/mainHolder.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'screens/general/screen_chef.dart';
+import 'screens/general/screen_home.dart';
+import 'screens/general/screen_list.dart';
+import 'screens/general/screen_meal_planner.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final GoRouter _router = GoRouter(
+    initialLocation: ScreenHome.routeName,
+    routes: [
+      GoRoute(
+        path: ScreenHome.routeName,
+        builder: (context, state) => ScreenHome(),
+      ),
+      GoRoute(
+        path: ChefScreen.routeName,
+        builder: (context, state) => ChefScreen(),
+      ),
+      GoRoute(
+        path: ScreenList.routeName,
+        builder: (context, state) => ScreenList(),
+      ),
+      GoRoute(
+        path: MealPlannerScreen.routeName,
+        builder: (context, state) => MealPlannerScreen(),
+      ),
+    ],
+  );
+
+  final ColorScheme kColorScheme = ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 96, 59, 181),
+  );
+
+  final ColorScheme kDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 5, 99, 125),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: _router,
+      title: 'My App',
+      theme: ThemeData(
+        colorScheme: kColorScheme,
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: kDarkColorScheme,
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
